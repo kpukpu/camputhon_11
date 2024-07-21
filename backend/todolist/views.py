7bb820b8382d7b0c9b4bf97358f6e48c4be20109
Hye0nw0O <hw4151@naver.com>
2024-07-21 19:34:42 +0900

    add name

--- backend/todolist/views.py
+++ backend/todolist/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import what_to_do
 import json
+from user.models import user
+
 # Create your views here.
 
 @api_view(['POST'])
@@ -13,7 +15,7 @@ def resist_to_do(request):
     _major = data.get('major')
 
     try:
-        nickname = 현우씨꺼모델명.objects.get(id=nickname_id)
+        nickname = user.objects.get(id=nickname_id)
         new_resist = what_to_do(
             nickname=nickname,
             hw_name=_hw_name,
