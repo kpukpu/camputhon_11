1f2e3b747288cb7a18967706c51f54bc0b4f260a
Esalyc <yunchancode@naver.com>
2024-07-21 23:55:07 +0900

    Login, Header, Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,19 @@ import Home from "./pages/Home";
 import Header from "./components/Header.js";
 import TestApi from "./pages/TestApi"
 import MyPage from "./pages/MyPage";
+import Footer from "./components/Footer";
 import "./index.css";
 
 function App() {
     return (
         <>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/calculate" element={<TestApi />} />
+                <Route path="/calendar" element={<TestApi />} />
                 <Route path="/my-page" element={<MyPage />} />
             </Routes>
+            <Footer />
         </>
     );
 }
