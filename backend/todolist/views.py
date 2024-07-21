b3c39473e9fff164dad78d9148438b806be4e0c6
kimseungju <daejo99@naver.com>
2024-07-21 19:24:43 +0900

    todolist real finish

--- backend/todolist/views.py
+++ backend/todolist/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-
+from rest_framework.response import Response
+from .models import what_to_do
+import json
 # Create your views here.
 
 @api_view(['POST'])
 def resist_to_do(request):
-    
+    data = json.loads(request.body)
+    nickname_id = data.get('nickname_id')
+    _hw_name = data.get('hw_name')
+    _major = data.get('major')
+
+    try:
+        nickname = 현우씨꺼모델명.objects.get(id=nickname_id)
+        new_resist = what_to_do(
+            nickname=nickname,
+            hw_name=_hw_name,
+            major=_major
+        )
+        new_resist.save()
+        return Response({'success': True, 'message': 'ToDo saved successfully.'})
+    except Exception as e:
+        return Response({'success': False, 'message': str(e)}, status=400)
