a598d087b2212aa6ba9be90162db48aa4a3ac517
Hye0nw0O <hw4151@naver.com>
2024-07-22 01:01:02 +0900

    add google id

--- backend/login/serializers.py
+++ backend/login/serializers.py
@@ -4,7 +4,7 @@ from .models import *
 class mypage_info(serializers.ModelSerializer): # 마이 페이지에 표시할 정보
     class Meta:
         model = GoogleUser
-        fields = ['nickname', 'banner', 'title', 'total_score', 'tier']
+        fields = ['nickname', 'banner', 'title', 'total_score', 'tier', 'google_id']
 
 class user_banner_title(serializers.ModelSerializer):
     class Meta:
